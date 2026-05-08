40e41e1946fa051dde69399a6fcf88778608c2de
ShafayatSadid <mdshafayathossainsadid@gmail.com>
2026-05-08 23:58:38 +0300

    featured section and tips section added

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,11 +1,12 @@
 import Featured from "@/components/Home/Featured";
 import Hero from "@/components/Home/Hero";
-
+import Tips from "@/components/Home/Tips"
 const HomePage = () => {
     return (
         <div>
             <Hero/>
-            
+            <Featured/>
+            <Tips/>
         </div>
     );
 };
---
+++ src/components/Home/TopBreeds.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopBreeds = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TopBreeds;
